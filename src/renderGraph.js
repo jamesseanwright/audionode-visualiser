aa1146a1f42569472dc2c849eab990398351bc35
James Wright <james@jamesswright.co.uk>
2017-06-28 16:43:55 +0100

    WIP rendering

--- src/renderGraph.js
+++ src/renderGraph.js
@@ -1,7 +1,13 @@
 'use strict';
 
-function renderGraph(/* targetElement */) {
+const d3 = require('d3-hierarchy');
+const graph = require('./graph/graph');
+const adaptToD3Hierarchy = require('./graph/adaptToD3Hierarchy');
 
+function renderGraph(/* targetElement */) {
+    const hierarchy = adaptToD3Hierarchy(graph);
+    const treemap = d3.treemap().size([window.innerWidth, window.innerHeight]).padding(2);
+    treemap(hierarchy);
 }
 
 module.exports = renderGraph;
