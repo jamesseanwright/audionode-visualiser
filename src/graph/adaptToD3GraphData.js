b74df78d02903027759cc149f7ee9bc9045eb52d
James Wright <james@jamesswright.co.uk>
2017-07-15 19:25:11 +0100

    WIP child node mapping

--- src/graph/adaptToD3GraphData.js
+++ src/graph/adaptToD3GraphData.js
@@ -1,15 +1,28 @@
 'use strict';
 
-function adaptToD3GraphData(node) {
-    const d3Node = {
+const initialData = {
+    nodes: [],
+    links: [],
+};
+
+function mapNode(node) {
+    return {
         name: node.toString()
     };
+}
+
+function adaptToD3GraphData(node, graphData = initialData) {
+    graphData.nodes = graphData.nodes.concat(mapNode(node));
+
+    const lastNodeIndex = data.nodes.length - 1;
 
     if (node.children) {
-        d3Node.children = node.children.map(adaptToD3GraphData);
+        graphData.nodes = graphData.nodes.concat(
+            node.children.map(mapNode)
+        );
     }
 
-    return d3Node;
+    return graphData;
 }
 
 module.exports = adaptToD3GraphData;
