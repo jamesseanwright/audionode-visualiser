947aa7578a706bca9cbd7c04edec94df4e370116
James Wright <james@jamesswright.co.uk>
2017-08-10 20:07:27 +0100

    Maintaining parent index between recursive graph adaptation

--- src/graph/adaptToD3GraphData.js
+++ src/graph/adaptToD3GraphData.js
@@ -14,20 +14,20 @@ function mapNode(node) {
 function createLink(parentIndex, childIndex) {
     return {
         source: parentIndex,
-        target: childIndex
+        target: childIndex,
     };
 }
 
-function adaptToD3GraphData(nodes, graphData = initialData, depth = 0) {
+function adaptToD3GraphData(nodes, graphData = initialData, parentIndex = 0) {
     for (let i = 0; i < nodes.length; i++) {
         const node = nodes[i];
-        console.log('*******', node._value, depth, i, graphData.nodes.length); // eslint-disable-line
+        const childIndex = graphData.nodes.length;
 
         graphData.nodes = graphData.nodes.concat(mapNode(node));
-        graphData.links = graphData.links.concat(createLink(depth + node.children.length, graphData.nodes.length));
+        graphData.links = graphData.links.concat(createLink(parentIndex, childIndex));
 
         if (node.children.length) {
-            adaptToD3GraphData(node.children, graphData, depth + 1);
+            adaptToD3GraphData(node.children, graphData, childIndex);
         }
     }
 
